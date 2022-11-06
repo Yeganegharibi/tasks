8243363781f2b4945011461062435ea5bcd78203
BasaCo <basa.co2022@gmail.com>
2022-11-06 08:23:11 +0330

    first commit

---
+++ yegane.cs
@@ -0,0 +1,12 @@
+using system;
+class booleans
+{
+    public static void main()
+    {
+        bool content = true;
+        bool nocontent = false;
+        console.writeline("text is{0}",content);
+        console.writeline("text is{0}",nocontent);
+    }
+
+}
